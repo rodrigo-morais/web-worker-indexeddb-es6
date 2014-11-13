64502e39e0ac41c47991327dee3d24c5eda2f4dd
Rodrigo Morais <morais.rm@gmail.com>
2014-11-13 09:25:50 -0200

    Config Grunt to transpile ES6 to vanilla ES5.

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+﻿module.exports = function (grunt) {
+
+    grunt.initConfig({
+        '6to5': {
+            options: {
+                modules: 'common'
+            },
+
+            build: {
+                files: [{
+                    expand: true,
+                    cwd: 'javascript/',
+                    src: ['main.js', 'indexedDB.js'],
+                    dest: 'dist/javascript',
+                }],
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-6to5');
+
+    grunt.registerTask('default', ['6to5']);
+
+};
